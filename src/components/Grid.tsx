78507862ebd3867a7d5b004553badaa7f3836dbc
rowanjames04 <r04james@outlook.com>
2026-02-14 22:06:31 +1100

    refactor(Grid): convert inline styles to responsive Tailwind CSS classes

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,30 +1,33 @@
+import { twMerge } from "tailwind-merge";
 import { usePathfinding } from "../hooks/usePathfinding";
 import { MAX_COLS, MAX_ROWS } from "../utils/constants";
-
-const TILE_SIZE = 20;
 
 export function Grid() {
     const {grid} = usePathfinding()
 
     return (
         <div
-            className="flex items-center flex-col justify-center border-sky-300 p-4"
-            style={{
-                width: MAX_COLS * TILE_SIZE,
-                minHeight: MAX_ROWS * TILE_SIZE,
-            }}
+            className={twMerge(
+                // base classes
+                "flex items-center flex-col justify-center border-sky-300",
+
+                // control grid height
+                `lg:min-h-[${MAX_ROWS * 17}px]
+                md:min-h-[${MAX_ROWS * 15}px]
+                xs:min-h-[${MAX_ROWS * 8}px]
+                min-h-[${MAX_ROWS * 7}px]`,
+
+                // control grid width
+                `lg:w-[${MAX_COLS * 17}px]
+                md:w-[${MAX_COLS * 15}px]
+                xs:w-[${MAX_COLS * 8}px]
+                w-[${MAX_COLS * 7}px]`
+            )}
         >
             {grid.map((row, rowIndex) => (
                 <div key={rowIndex} className="flex">
                     {row.map((tile, tileIndex) => (
-                        <div
-                            key={tileIndex}
-                            className={`border ${tile.isStart ? 'bg-green-500' : tile.isEnd ? 'bg-red-500' : tile.isWall ? 'bg-gray-800' : 'bg-white'}`}
-                            style={{
-                                width: TILE_SIZE - 1,
-                                height: TILE_SIZE - 1,
-                            }}
-                        />
+                        <div className="bg-white h-2 w-2 border" />
                     ))}
                 </div>
             ))}
