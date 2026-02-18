1a12d3d473b03be4a6c85abed6129f0a16ff5679
rowanjames04 <r04james@outlook.com>
2026-02-18 22:09:08 +1100

    feat: add Tile component with state-based styling for pathfinding grid
    Created a new Tile component to render grid cells with appropriate styles based on their state (start, end, wall, path, traversed). Updated Grid.tsx to use the new component and added tile style constants in constants.ts.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -11,7 +11,7 @@ export const START_TILE_CONFIGURATION = {
     isStart: false,
     isTraversed: false,
     parent: null
-}
+};
 
 export const END_TILE_CONFIGURATION = {
     row: MAX_ROWS - 2,
@@ -23,4 +23,13 @@ export const END_TILE_CONFIGURATION = {
     isStart: false,
     isTraversed: false,
     parent: null
-}
+};
+
+export const TILE_STYLE = 
+    "lg:w-[17px] md:w-[15px] xs:w-[8px] w-[7px] lg:h[17px] md:h-[15px] xs:h-[8px] h-[7px] border-t border-r border-sky-200";
+
+export const TRAVERSED_TILE_STYLE = TILE_STYLE + " bg-cyan-400"
+export const START_TILE_STYLE = TILE_STYLE + " bg-green-400"
+export const END_TILE_STYLE = TILE_STYLE + " bg-red-400"
+export const WALL_TILE_STYLE = TILE_STYLE + " bg-gray-400"
+export const PATH_TILE_STYLE = TILE_STYLE + " bg-green-500"
